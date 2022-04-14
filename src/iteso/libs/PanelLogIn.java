5e926ddff39486a4069e4d574d08f30d444d63c4
Pablo Vergara <vergarapablo2001@gmail.com>
2022-04-13 22:06:27 -0500

    antes de añadir usuarios.txt

--- src/iteso/libs/PanelLogIn.java
+++ src/iteso/libs/PanelLogIn.java
@@ -11,21 +11,25 @@ import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 
 public class PanelLogIn extends JFrame implements ActionListener {
-	
+
 	private final int PANEL_WIDTH = 400, PANEL_HEIGHT = 300;
-	
+
 	JPanel jpPanel;
-	JLabel jlTitle, jlUserName, jlPassword;
-	JTextField jtfUserName, jtfPassword;
+	JLabel jlTitle, jlUserName, jlPassword, jlError;
+	JTextField jtfUserName;
 	JButton jbLogIn;
-	
+	JCheckBox jcbShowPassword;
+
+	JPasswordField jpfPassword;
+
 	public PanelLogIn() {
-		
+
 		// Create JPanel
 		jpPanel = new JPanel();
 		jpPanel.setLayout(null);
@@ -37,61 +41,88 @@ public class PanelLogIn extends JFrame implements ActionListener {
 		setResizable(false);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
-		
+
 		// Create Font
 		Font font1 = new Font("Arial", 1, 35);
 		Font font2 = new Font("Arial", 1, 20);
 		Font font3 = new Font("Arial", 1, 15);
 		Font font4 = new Font("Arial", 0, 20);
-		
+		Font font5 = new Font("Arial", 0, 15);
+
 		// Create JLabel
 		jlTitle = new JLabel("Log In");
 		jlTitle.setFont(font1);
 		jlTitle.setBounds(149, 15, 102, 45);
-		jlTitle.setOpaque(true);
 		jpPanel.add(jlTitle);
-		
+
 		jlUserName = new JLabel("User Name:");
 		jlUserName.setFont(font2);
 		jlUserName.setBounds(20, 75, 110, 30);
-		jlUserName.setOpaque(true);
 		jpPanel.add(jlUserName);
-		
+
 		jlPassword = new JLabel("Password:");
 		jlPassword.setFont(font2);
 		jlPassword.setBounds(30, 115, 100, 30);
-		jlPassword.setOpaque(true);
 		jpPanel.add(jlPassword);
-		
+
+		jlError = new JLabel("Error");
+		jlError.setFont(font3);
+		jlError.setForeground(Color.red);
+		jlError.setBounds(140, 185, 200, 30);
+		jlError.setVisible(false);
+		jpPanel.add(jlError);		
+
 		// Create JButton
 		jbLogIn = new JButton("Login");
 		jbLogIn.setFont(font3);
 		jbLogIn.setBounds(160, 230, 80, 20);
 		jbLogIn.addActionListener(this);
 		jpPanel.add(jbLogIn);
-		
+
 		// Create JTextField
 		jtfUserName = new JTextField();
 		jtfUserName.setFont(font4);
 		jtfUserName.setBounds(135, 75, 225, 30);
 		jpPanel.add(jtfUserName);
-		
-		// Create JPasswordField
-		JPasswordField jtfPassword = new JPasswordField();
-		jtfPassword.setFont(font4);
-		jtfPassword.setBounds(135, 115, 225, 30);
-		jtfPassword.setEchoChar('*');
-        jpPanel.add(jtfPassword);
 
-		
-		
+		// Create JPasswordField
+		jpfPassword = new JPasswordField();
+		jpfPassword.setFont(font4);
+		jpfPassword.setEchoChar('*');	
+		jpfPassword.setBounds(135, 115, 225, 30);
+		jpPanel.add(jpfPassword);
+
+		// Create JCheckBox
+		jcbShowPassword = new JCheckBox("Show Password");
+		jcbShowPassword.setFont(font5);
+		jcbShowPassword.setBounds(135, 150, 225, 30);
+		jcbShowPassword.addActionListener(this);
+		jpPanel.add(jcbShowPassword);
+
+	}
+
+	private void logIn() {
+		if (jtfUserName.getText().length() <= 0) {
+			jlError.setText("Username is empty");
+			jlError.setVisible(true);
+		}
+		else {
+			System.out.println("User Name: "+ jtfUserName.getText());
+			char[] password = jpfPassword.getPassword();
+			System.out.println("Password: "+String.valueOf(password));	
+		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == jbLogIn) {
-			System.out.println("User Name: "+ jtfUserName.getText());
+			logIn();		
 		}
+
+		if (jcbShowPassword.isSelected())
+			jpfPassword.setEchoChar((char)0);
+		else
+			jpfPassword.setEchoChar('*');
 		
 	}
 
