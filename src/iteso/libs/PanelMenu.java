3f14ac56ce1d5a4aff8212b60caf710d412649a7
Pablo Vergara <vergarapablo2001@gmail.com>
2022-04-14 11:37:17 -0500

    inicio de clase PanelMenu y pre edicion clase Users

---
+++ src/iteso/libs/PanelMenu.java
@@ -0,0 +1,16 @@
+package iteso.libs;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+
+public class PanelMenu {
+	
+	JPanel jpPanel;
+	JButton jbReturn, jbSettings, jbInventoryManagement, jbInventory, jbRecord, jbStatistics, jbAccountManager;
+	
+	public PanelMenu(int accountType) {
+		
+	}
+}
