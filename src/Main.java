8c7cb0f3581624526ec6614225a0918e3c0f6caf
Pablo Vergara <vergarapablo2001@gmail.com>
2022-04-16 00:33:11 -0500

    ya jala la lectura del inventario.txt

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		//		PanelLogIn.createPanelLogIn();
-		//		PanelMenu.createPanelMenu(4);
+//				PanelLogIn.createPanelLogIn();
+//				PanelMenu.createPanelMenu(4);
 
 		
 		
@@ -17,18 +17,7 @@ public class Main {
 			}
 			System.out.println();
 		}
-		
-//		String[][] information = {{"0 0", "0 1", "0 2"},{"1 0", "1 1", "1 2"},{"2 0", "2 1", "2 2"}};
-//		String[] toAdd = {"3 0", "3 1", "3 2"};
-//		
-//		information = Inventory.addElementArray(information, toAdd);
-//		
-//		for (int i = 0; i < information.length; i++) {
-//			for (int j = 0; j < information[i].length; j++) {
-//				System.out.print(information[i][j] + ", ");
-//			}
-//			System.out.println();
-//		}
+
 
 
 	}
--- src/iteso/libs/Inventory.java
+++ src/iteso/libs/Inventory.java
@@ -25,39 +25,16 @@ public class Inventory {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static String[][] addElementArray(String[][] array, String[] toAdd){
-		System.out.println("\n\n Como llega array a la funcion");
-		
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				System.out.print(array[i][j] + ", ");
-			}
-			System.out.println();
-		}
 		
 		String[][] newArray = new String[array.length+1][];
 		for(int i = 0; i< array.length; i++) {
 			newArray[i] = array[i];
-			System.out.println("\n\ndentro del for newArray");
-			for (int j = 0; j < 5; j++) System.out.print(newArray[i][j] + ", ");
-			System.out.println("\n\ndentro del for array");
-			for (int j = 0; j < 5; j++) System.out.print(array[i][j] + ", ");
 		}
-		
-		System.out.println("\n\ndentro de addElementArray");
-//		for (int i = 0; i < 5; i++) System.out.print(toAdd[i] + ", ");
 		
 		newArray[array.length] = toAdd;
-		
-		for (int i = 0; i < newArray.length; i++) {
-			for (int j = 0; j < newArray[i].length; j++) {
-				System.out.print(newArray[i][j] + ", ");
-			}
-			System.out.println();
-		}
-		
-		//newArray[array.length] = toAdd;
+
 		return newArray;
 	}
 
@@ -91,39 +68,17 @@ public class Inventory {
 		// no empty		
 		lineCounter = 0;
 		String[] toAdd = new String[5];
+		String presta;
 		try {
 			File myObj = new File("inventory.txt");
 			Scanner myReader = new Scanner(myObj);
 			while (myReader.hasNextLine()) {
-				toAdd[lineCounter%5] = myReader.nextLine().toString();
-				if (lineCounter%5 == 4) {
-					System.out.println("\n\nLo que se queire añadir");
-					for (int i = 0; i < 5; i++) System.out.print(toAdd[i] + ", ");
-					System.out.println();
-					System.out.println("\n\nAntes de ir a la funcion");
-					for (int i = 0; i < information.length; i++) {
-						for (int j = 0; j < information[i].length; j++) {
-							System.out.print(information[i][j] + ", ");
-						}
-						System.out.println();
-					}
-					
+				presta = myReader.nextLine();
+				toAdd[lineCounter%5] = presta;
+				if (lineCounter%5 == 4) {						
 					information = Inventory.addElementArray(information, toAdd);
-					
-					System.out.println("\n\nComo sale de la funcion");
-					for (int i = 0; i < information.length; i++) {
-						for (int j = 0; j < information[i].length; j++) {
-							System.out.print(information[i][j] + ", ");
-						}
-						System.out.println();
-					}
-					
-					System.out.println();
-					System.out.println();
+					toAdd = new String[5];
 				}
-
-
-
 				lineCounter++;
 			}
 			myReader.close();
